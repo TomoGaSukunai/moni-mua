1ea3df999b84d3b729ac7949decf485be7b5f505
pengzhanbo <q942450674@outlook.com>
2021-05-30 00:33:04 +0800

    feat: github page

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'docs'
+  },
   plugins: [vue(), legacy({
     targets: ['defaults', 'not IE 11']
   })]
